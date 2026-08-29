e7c006d42d39a913297c71c8c15cbb9a103f1473
Kaustubh Patil <117383257+Anonymouskau@users.noreply.github.com>
2026-08-29 09:20:55 +0530

    Update plot.r

--- plot.r
+++ plot.r
@@ -1,1 +1,43 @@
 
+library(ggplot2)
+data(mtcars)
+mtcars$cyl <- factor(mtcars$cyl)
+mtcars$am <- factor(mtcars$am,
+                    levels = c(0, 1),
+                    labels = c("Automatic", "Manual"))
+
+
+
+View(mtcars)
+
+
+#Scatter plot
+plot1=ggplot(mtcars, aes(x=wt, y=mpg)) +xlab(" wt (car
+weight)")+ylab("mpg (miles per gallon)")+
+  geom_point(shape=1) + ggtitle("Scatter plot wt vs mpg")
+
+plot1
+#Scatter plot2
+plot2=ggplot(mtcars, aes(x=wt, y=mpg,colour = am)) +xlab(" wt (car
+weight)")+ylab("mpg (miles per gallon)")+
+  geom_point(shape=1) + ggtitle("Scatter plot wt vs mpg")
+
+plot2
+
+
+
+
+
+#histogram
+plot3=ggplot(mtcars, aes(x=mpg )) + xlab("Mpg")+
+  geom_histogram(bins = 10, colour="black", fill="white")+ggtitle("Distribution of mpg")
+
+plot3
+
+#boxplot
+bp <- ggplot(mtcars, aes(x=cyl, y=mpg, fill=cyl)) + xlab("No of Cylinder")+ylab("Miles per gallon")+
+  geom_boxplot()+ggtitle(" Box Plot Cylinders Vs Mpg")
+bp
+
+
+
